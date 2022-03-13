23ec8cd27ad1dbed27d0a7a2c1b9c3521ab44f01
Hayel <ivanforwork@yandex.ru>
2022-03-13 03:45:23 +0300

    Filters added.

---
+++ src/main/java/by/teachmeskills/homework/web/filter/AdminFilter.java
@@ -0,0 +1,29 @@
+package by.teachmeskills.homework.web.filter;
+
+import by.teachmeskills.homework.entity.User;
+import by.teachmeskills.homework.web.constant.attribute.SessionAttribute;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "AdminFilter", servletNames = "AdminServlet")
+public class AdminFilter implements Filter {
+    @Override
+    @SneakyThrows({IOException.class, ServletException.class})
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        @NonNull HttpSession session = req.getSession();
+        User currentUser = (User) session.getAttribute(SessionAttribute.USER.get());
+        if (currentUser != null && currentUser.isAdmin())
+            chain.doFilter(req, resp);
+        else
+            resp.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
---
+++ src/main/java/by/teachmeskills/homework/web/filter/AuthorizationFilter.java
@@ -0,0 +1,31 @@
+package by.teachmeskills.homework.web.filter;
+
+import by.teachmeskills.homework.entity.User;
+import by.teachmeskills.homework.web.constant.attribute.SessionAttribute;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+
+import java.io.IOException;
+
+@WebFilter(filterName = "AuthorizationFilter", servletNames = {
+        "PostServlet", "PostHistoryServlet", "CommentServlet", "UserServlet", "AdminServlet", "LogoutServlet"
+})
+public class AuthorizationFilter implements Filter {
+    @Override
+    @SneakyThrows({IOException.class, ServletException.class})
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
+        @NonNull HttpSession session = req.getSession();
+        User currentUser = (User) session.getAttribute(SessionAttribute.USER.get());
+        if (currentUser != null)
+            chain.doFilter(req, resp);
+        else
+            resp.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
